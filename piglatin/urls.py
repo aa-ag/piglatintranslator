5d35f450441a6886c5b96d979daf3eb366bdd2e1
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-20 16:22:28 -0500

    go back button

--- piglatin/urls.py
+++ piglatin/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', views.home, name='home'),
     path('translate/', views.translate, name='translate'),
 ]
