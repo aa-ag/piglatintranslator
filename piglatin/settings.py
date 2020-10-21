332466eb6a3f1001cff08152bd0a7d8e10eb776c
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-21 16:19:40 -0500

    required input

--- piglatin/settings.py
+++ piglatin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'sut-hp_h_abypy&xgr(01wg1x)=^y0gi-^7s^1l&o!!$9)mlnc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
